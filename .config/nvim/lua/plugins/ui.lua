3f2b994a01169a33a3323a4915a3733f322b74cd
Pax-Newman <newmanp@wwu.edu>
2024-03-07 10:49:44 -0800

    added nvim :)

---
+++ .config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,40 @@
+return {
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'seoul256',
+        component_separators = '|',
+        section_separators = { left = '', right = '' },
+      },
+      sections = {
+        lualine_a = {
+          { 'mode', separator = { left = '' }, right_padding = 2 },
+        },
+        lualine_b = { 'filename', 'branch' },
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = { 'filetype', 'progress' },
+        lualine_z = {
+          { 'location', separator = { right = '' }, left_padding = 2 },
+        },
+      },
+      inactive_sections = {
+        lualine_a = { 'filename' },
+        lualine_b = {},
+        lualine_c = {},
+        lualine_x = {},
+        lualine_y = {},
+        lualine_z = { 'location' },
+      },
+      tabline = {},
+      extensions = {},
+    },
+    init = function()
+      vim.o.laststatus = 3
+    end
+  },
+}
