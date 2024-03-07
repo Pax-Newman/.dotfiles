8a4a7393e9e8acc2950507e6de8e398092b12342
Pax-Newman <pax.newman@gmail.com>
2024-03-07 12:28:53 -0800

    configured stylua, moved options + some plugins to extern files

--- .config/nvim/lua/plugins/fterm.lua
+++ .config/nvim/lua/plugins/fterm.lua
@@ -1,25 +1,25 @@
 return {
-  -- Integrated floating terminal
+   -- Integrated floating terminal
 
-  {
-    'numToStr/FTerm.nvim',
-    config = function ()
-      require('FTerm').setup {
-        border = 'rounded',
-        dimensions = {
-          height = 0.8,
-          width = 0.7,
-        },
-      }
-      -- Remap tabs back to their original behavior
-      -- TODO: See if there's a better way of doing this
-      vim.keymap.set('n', '<tab>', '<tab>')
-      vim.keymap.set('v', '<tab>', '<tab>')
-      vim.keymap.set('t', '<tab>', '<tab>')
-      -- toggle term in normal mode
-      vim.keymap.set('n', '<C-i>', '<CMD>lua require("FTerm").toggle()<CR>')
-      -- toggle term in terminal mode
-      vim.keymap.set('t', '<C-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
-    end
-  },
+   {
+      "numToStr/FTerm.nvim",
+      config = function()
+         require("FTerm").setup({
+            border = "rounded",
+            dimensions = {
+               height = 0.8,
+               width = 0.7,
+            },
+         })
+         -- Remap tabs back to their original behavior
+         -- TODO: See if there's a better way of doing this
+         vim.keymap.set("n", "<tab>", "<tab>")
+         vim.keymap.set("v", "<tab>", "<tab>")
+         vim.keymap.set("t", "<tab>", "<tab>")
+         -- toggle term in normal mode
+         vim.keymap.set("n", "<C-i>", '<CMD>lua require("FTerm").toggle()<CR>')
+         -- toggle term in terminal mode
+         vim.keymap.set("t", "<C-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+      end,
+   },
 }
