3ec45d23e86b688a9cf2318f2d3bacf15b3ad638
Pax-Newman <newmanp@wwu.edu>
2024-03-24 22:39:40 -0700

    added icons to completions menu

--- .config/nvim/lua/plugins/completion.lua
+++ .config/nvim/lua/plugins/completion.lua
@@ -42,6 +42,34 @@ return { -- Autocompletion
 
       luasnip.config.setup {}
 
+      local kind_icons = {
+         Text = '',
+         Method = '󰆧',
+         Function = '󰊕',
+         Constructor = '',
+         Field = '󰇽',
+         Variable = '󰂡',
+         Class = '󰠱',
+         Interface = '',
+         Module = '',
+         Property = '󰜢',
+         Unit = '',
+         Value = '󰎠',
+         Enum = '',
+         Keyword = '󰌋',
+         Snippet = '',
+         Color = '󰏘',
+         File = '󰈙',
+         Reference = '',
+         Folder = '󰉋',
+         EnumMember = '',
+         Constant = '󰏿',
+         Struct = '',
+         Event = '',
+         Operator = '󰆕',
+         TypeParameter = '󰅲',
+      }
+
       cmp.setup {
          snippet = {
             expand = function(args)
@@ -93,6 +121,22 @@ return { -- Autocompletion
             { name = 'nvim_lsp' },
             { name = 'luasnip' },
             { name = 'path' },
+         },
+
+         formatting = {
+            format = function(entry, vim_item)
+               -- Kind icons
+               vim_item.kind = string.format('%s %s', kind_icons[vim_item.kind], vim_item.kind) -- This concatenates the icons with the name of the item kind
+               -- Source
+               -- vim_item.menu = ({
+               --    buffer = '[Buffer]',
+               --    nvim_lsp = '[LSP]',
+               --    luasnip = '[LuaSnip]',
+               --    nvim_lua = '[Lua]',
+               --    latex_symbols = '[LaTeX]',
+               -- })[entry.source.name]
+               return vim_item
+            end,
          },
       }
    end,
