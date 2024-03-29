0dc9608a7f20368e0c2881bc2b4992665a81ba60
Pax-Newman <pax.newman@gmail.com>
2024-03-29 10:07:25 -0700

    set config tabwidth to 3, cleaned things up a bit

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,38 +1,37 @@
 #Pax Newman FISH Config
 
 if status is-interactive
-    # Commands to run in interactive sessions can go here
-    #set -U fish_greeting # comment out to disable the motd?
-    
-    # default to the manga dir if there's nothing set to MOTD_DIRS
-    if test -z "$MOTD_DIRS" 
-        set -a MOTD_DIRS "manga"
-    end
-    # add all files from each dir in MOTD_DIRS
-    for dir in $MOTD_DIRS
-        set -af pics (printf "%s" ~/.config/motd_pictures/$dir/)*.*
-    end
-    # Display a random picture from $pics
-    if test -n "$ZELLIJ"
-        img2sixel (random choice $pics) --height 512
-    else
-        itermimg (random choice $pics) --height 50%
-    end
+   # Commands to run in interactive sessions can go here
+   #set -U fish_greeting # comment out to disable the motd?
 
-    alias c='z'
+   # default to the manga dir if there's nothing set to MOTD_DIRS
+   if test -z "$MOTD_DIRS" 
+      set -a MOTD_DIRS "manga"
+   end
+   # add all files from each dir in MOTD_DIRS
+   for dir in $MOTD_DIRS
+      set -af pics (printf "%s" ~/.config/motd_pictures/$dir/)*.*
+   end
+   # Display a random picture from $pics
+   if test -n "$ZELLIJ"
+      img2sixel (random choice $pics) --height 512
+   else
+      itermimg (random choice $pics) --height 50%
+   end
 
-    alias gs='git status'
-    alias ga='git add'
-    alias gc='git commit -m'
-    alias gd='git diff'
+   alias c='z'
+
+   alias gs='git status'
+   alias ga='git add'
+   alias gc='git commit -m'
+   alias gd='git diff'
 
 
-    alias dots='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+   alias dots='/usr/bin/git --git-dir="$HOME/.dotfiles/" --work-tree="$HOME"'
+   alias dotadd='dots add'
+   alias dotstat='dots status'
 
-    alias dotadd='dots add'
-    alias dotstat='dots status'
-
-    fish_config theme choose 'Rosé Pine Moon'
+   fish_config theme choose 'Rosé Pine Moon'
 end
 
 set -gx EDITOR nvim
@@ -57,6 +56,8 @@ fish_add_path /opt/homebrew/bin
 
 # -- PATH
 fish_add_path --path /Users/paxnewman/.cargo/bin
+
+# -- Fish Integrations
 
 test -e {$HOME}/.iterm2_shell_integration.fish ; and source {$HOME}/.iterm2_shell_integration.fish
 
