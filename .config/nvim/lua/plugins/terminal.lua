b0ddc454afb0d7458cd9bf233d4900120edd9bb4
Pax-Newman <pax.newman@gmail.com>
2024-04-18 13:41:24 -0700

    nvim: terminal uses $SHELL to determine what shell to use

--- .config/nvim/lua/plugins/terminal.lua
+++ .config/nvim/lua/plugins/terminal.lua
@@ -2,24 +2,25 @@ return {
    -- Integrated floating terminal
 
    {
-      'numToStr/FTerm.nvim',
+      "numToStr/FTerm.nvim",
       config = function()
-         require('FTerm').setup {
-            border = 'rounded',
+         require("FTerm").setup({
+            border = "rounded",
             dimensions = {
                height = 0.8,
                width = 0.7,
             },
-         }
+            cmd = os.getenv("SHELL"),
+         })
          -- Remap tabs back to their original behavior
          -- TODO: See if there's a better way of doing this
-         vim.keymap.set('n', '<tab>', '<tab>')
-         vim.keymap.set('v', '<tab>', '<tab>')
-         vim.keymap.set('t', '<tab>', '<tab>')
+         vim.keymap.set("n", "<tab>", "<tab>")
+         vim.keymap.set("v", "<tab>", "<tab>")
+         vim.keymap.set("t", "<tab>", "<tab>")
          -- toggle term in normal mode
-         vim.keymap.set('n', '<C-i>', '<CMD>lua require("FTerm").toggle()<CR>')
+         vim.keymap.set("n", "<C-i>", '<CMD>lua require("FTerm").toggle()<CR>')
          -- toggle term in terminal mode
-         vim.keymap.set('t', '<C-i>', '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
+         vim.keymap.set("t", "<C-i>", '<C-\\><C-n><CMD>lua require("FTerm").toggle()<CR>')
       end,
    },
 
