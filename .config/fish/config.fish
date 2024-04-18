dcb6bb937386852024219adac96ac2bdcf172bed
Pax-Newman <pax.newman@gmail.com>
2024-04-18 11:16:42 -0700

    fish: fixed weather fetching to only update the weather every 15 minutes to reduce server load

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
 #Pax Newman FISH Config
 
+# ---- Important EnvVars
+set -gx XDG_DATA_HOME $HOME/.local/share
+
 # Commands to run in interactive sessions can go here
 if status is-interactive
 
@@ -18,11 +21,27 @@ if status is-interactive
       else
          itermimg (random choice $pics) --height 50%
       end
-   # Otherwise display a text message
+
+   # Otherwise display a text message with the weather
    else
-      # Get the weather
+      set -l wttr_last $XDG_DATA_HOME/weather/last
+      set -l wttr_data $XDG_DATA_HOME/weather/data
+
+      if not test -e $wttr_last
+         mkdir -p $XDG_DATA_HOME/weather/
+         echo 0 > $wttr_last
+      end
+
+      # Check if it's been 15 minutes since we last checked the weather
+      if test (math (date +%s) - (cat $wttr_last)) -gt 900
+         date +%s > $wttr_last
+
+         # Update the weather
+         curl --silent 'wttr.in/?format=%l\n%t+%c\nprecipitation:%20%p\n' -o $wttr_data > /dev/null
+      end
+
       date +'%a. - %b: %d'
-      curl 'wttr.in/?format=%l\n%t+%c\nprecipitation:%20%p\n'
+      cat $wttr_data
    end
 
    alias c='z'
@@ -46,7 +65,6 @@ set -g fish_term24bit 1
 
 set -gx BAT_THEME ansi
 
-set -gx XDG_DATA_HOME $HOME/.local/share
 
 # Rust CLI App inits
 zoxide init fish | source
