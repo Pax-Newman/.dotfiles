353c538cef5ea3c2a7218c3acede8d6c88253655
Pax-Newman <newmanp@wwu.edu>
2024-05-07 18:06:26 -0700

    nvim: added command to disable/enable autoformattingo

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -424,7 +424,14 @@ require('lazy').setup({
                      css = true,
                      javascript = true,
                   },
-                  provideFormatter = false,
+                  provideFormatter = true,
+               },
+               settings = {
+                  html = {
+                     format = {
+                        unformattedContentDelimiter = '<!-- noformat -->',
+                     },
+                  },
                },
             },
          }
@@ -464,10 +471,26 @@ require('lazy').setup({
       'stevearc/conform.nvim',
       opts = {
          notify_on_error = false,
-         format_on_save = {
-            timeout_ms = 500,
-            lsp_fallback = true,
-         },
+
+         format_on_save = function(bufnr)
+            -- Disable autoformat on certain filetypes
+            local ignore_filetypes = { 'sql', 'java' }
+            if vim.tbl_contains(ignore_filetypes, vim.bo[bufnr].filetype) then
+               return
+            end
+            -- Disable with a global or buffer-local variable
+            if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+               return
+            end
+            -- Disable autoformat for files in a certain path
+            local bufname = vim.api.nvim_buf_get_name(bufnr)
+            if bufname:match '/node_modules/' then
+               return
+            end
+            -- ...additional logic...
+            return { timeout_ms = 500, lsp_fallback = true }
+         end,
+
          formatters_by_ft = {
             lua = { 'stylua' },
             json = { 'jq' },
@@ -476,8 +499,9 @@ require('lazy').setup({
             --
             -- You can use a sub-list to tell conform to run *until* a formatter
             -- is found.
-            -- javascript = { { "prettierd", "prettier" } },
+            javascript = { { 'prettierd', 'prettier' } },
          },
+
          formatters = {
             stylua = {
                command = 'stylua',
@@ -493,6 +517,27 @@ require('lazy').setup({
             },
          },
       },
+      config = function(self, opts)
+         require('conform').setup(opts)
+
+         vim.api.nvim_create_user_command('FormatDisable', function(args)
+            if args.bang then
+               -- FormatDisable! will disable formatting just for this buffer
+               vim.b.disable_autoformat = true
+            else
+               vim.g.disable_autoformat = true
+            end
+         end, {
+            desc = 'Disable autoformat-on-save',
+            bang = true,
+         })
+         vim.api.nvim_create_user_command('FormatEnable', function()
+            vim.b.disable_autoformat = false
+            vim.g.disable_autoformat = false
+         end, {
+            desc = 'Re-enable autoformat-on-save',
+         })
+      end,
    },
 
    { -- You can easily change to a different colorscheme.
