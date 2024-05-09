dcf3c0853c7c853c2b089596ffefb2fcdc8c6be7
Pax-Newman <newmanp@wwu.edu>
2024-05-09 14:28:32 -0700

    nvim: added neorg

---
+++ .config/nvim/lua/plugins/neorg.lua
@@ -0,0 +1,29 @@
+return {
+   {
+      'nvim-neorg/neorg',
+      dependencies = { 'luarocks.nvim' },
+      -- Disable lazy loading as some `lazy.nvim` distributions set `lazy = true` by default
+      lazy = false,
+      -- Pin Neorg to the latest stable release
+      version = '*',
+      opts = {
+         load = {
+            ['core.defaults'] = {},
+            ['core.concealer'] = {},
+            ['core.dirman'] = {
+               config = {
+                  workspaces = {
+                     notes = '~/notes',
+                  },
+                  default_workspace = 'notes',
+               },
+            },
+         },
+      },
+   },
+   {
+      'vhyrro/luarocks.nvim',
+      priority = 1000,
+      config = true,
+   },
+}
