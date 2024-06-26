e4432e3bc0c42130d3f5a1b983de85c2a64bf96c
Pax-Newman <newmanp@wwu.edu>
2024-06-25 23:52:05 -0700

    fish: weather.fish refactored to require only one data file

--- .config/fish/functions/weather.fish
+++ .config/fish/functions/weather.fish
@@ -1,21 +1,28 @@
 function weather
-   set -l wttr_last $XDG_DATA_HOME/weather/last
-   set -l wttr_data $XDG_DATA_HOME/weather/data
+   set -g wttr_data $XDG_DATA_HOME/weather/data
 
-   if not test -e $wttr_last
+   function __fetch
+      curl \
+         --silent \
+         'wttr.in/?format=%l\n%t+%c\nprecipitation:%20%p\n' \
+         -o $wttr_data
+   end
+
+   if not test -e $wttr_data
       mkdir -p $XDG_DATA_HOME/weather/
-      echo 0 > $wttr_last
+      __fetch
    end
 
    # Check if it's been 15 minutes since we last checked the weather
-   if test (math (date +%s) - (cat $wttr_last)) -gt 900
-      date +%s > $wttr_last
+   if test (math (date +%s) - (date -r $wttr_data +%s)) -gt 900
 
       # Update the weather
-      curl --silent 'wttr.in/?format=%l\n%t+%c\nprecipitation:%20%p\n' \
-         -o $wttr_data
+      __fetch
    end
 
    date +'%A %b. %d'
    cat $wttr_data
+
+   # Cleanup
+   set -e wttr_data
 end
