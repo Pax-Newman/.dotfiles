0c927c71416af201f2855486262047598a645dca
Pax-Newman <newmanp@wwu.edu>
2024-07-01 23:19:36 -0700

    nvim: added xcode tools integration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -11,7 +11,19 @@ return {
 
          -- Useful status updates for LSP.
          -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-         { 'j-hui/fidget.nvim', opts = {} },
+         {
+            'j-hui/fidget.nvim',
+            opts = {
+               integration = {
+                  ['nvim-tree'] = {
+                     enable = true, -- Integrate with nvim-tree/nvim-tree.lua (if installed)
+                  },
+                  ['xcodebuild-nvim'] = {
+                     enable = false, -- Integrate with wojciech-kulik/xcodebuild.nvim (if installed)
+                  },
+               },
+            },
+         },
       },
       config = function()
          -- Brief Aside: **What is LSP?**
