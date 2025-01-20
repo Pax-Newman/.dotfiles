3cd963e0f387c8e576eaa027463c31a4f41e939d
Pax-Newman <newmanp@wwu.edu>
2025-01-19 21:49:21 -0800

    fish: using `setpics` without any arguments will erase any previously set motd directories

--- .config/fish/functions/setpics.fish
+++ .config/fish/functions/setpics.fish
@@ -1,4 +1,9 @@
 function setpics
+   # Clear the current env var
    set --erase MOTD_DIRS
-   set -Ua MOTD_DIRS $argv
+
+   # If we received any arguments then put them in the env var
+   if test (count $argv) -gt 0
+      set -Ua MOTD_DIRS $argv
+   end
 end
