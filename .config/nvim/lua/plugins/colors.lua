d4ba1f09ce337cd27eef00c6fb44ac06f9ad8c53
Pax-Newman <pax.newman@gmail.com>
2025-05-28 11:58:34 -0700

    nvim: set evergarden-winter to default colorscheme

--- .config/nvim/lua/plugins/colors.lua
+++ .config/nvim/lua/plugins/colors.lua
@@ -9,15 +9,6 @@ return {
       priority = 1000, -- make sure to load this before all the other start plugins
       variant = 'moon',
       name = 'rose-pine',
-      config = function()
-         -- Load the colorscheme here.
-         -- Like many other themes, this one has different styles, and you could load
-         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-         vim.cmd.colorscheme 'rose-pine'
-
-         -- You can configure highlights by doing something like
-         vim.cmd.hi 'Comment gui=none'
-      end,
    },
 
    {
@@ -26,11 +17,11 @@ return {
       priority = 1000, -- Colorscheme plugin is loaded first before any other plugins
       opts = {
          theme = {
-            variant = 'fall', -- 'winter'|'fall'|'spring'|'summer'
+            variant = 'winter', -- 'winter'|'fall'|'spring'|'summer'
             accent = 'green',
          },
          editor = {
-            transparent_background = false,
+            transparent_background = true,
             sign = { color = 'none' },
             float = {
                color = 'mantle',
@@ -41,5 +32,14 @@ return {
             },
          },
       },
+      config = function()
+         -- Load the colorscheme here.
+         -- Like many other themes, this one has different styles, and you could load
+         -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+         vim.cmd.colorscheme 'evergarden'
+
+         -- You can configure highlights by doing something like
+         vim.cmd.hi 'Comment gui=none'
+      end,
    },
 }
