d41cf80c81ebf4bb24259919dabe0fbe8b60e124
Pax Newman <pax.newman@gmail.com>
2026-04-21 15:09:06 -0700

    nvim: fixed lsp configs to default to cwd as root and added ruff lsp

--- .config/nvim/lsp/pyrefly.lua
+++ .config/nvim/lsp/pyrefly.lua
@@ -3,13 +3,16 @@ return {
    cmd = { "pyrefly", "lsp" },
    filetypes = { "python" },
    root_markers = {
-      "pyrefly.toml",
-      "pyproject.toml",
-      "setup.py",
-      "setup.cfg",
-      "requirements.txt",
-      "Pipfile",
-      ".git",
+      {
+         "pyrefly.toml",
+         "pyproject.toml",
+         "setup.py",
+         "setup.cfg",
+         "requirements.txt",
+         "Pipfile",
+         ".git",
+      },
+      ".",
    },
    on_exit = function(code, _, _)
       vim.schedule(function()
